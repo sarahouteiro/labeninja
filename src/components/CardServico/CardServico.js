eb1e6aeb5659b46b051d68a3e27d9d341af68e15
Sarah Outeiro <sarahdinizouteiro@mgmail.com>
2022-06-30 15:56:45 -0300

    projeto mergeado

--- src/components/CardServico/CardServico.js
+++ src/components/CardServico/CardServico.js
@@ -2,6 +2,19 @@ import React from "react";
 import { CardsServico } from "./styled";
 
 export default class CardServico extends React.Component {
+    handleAdicionarAoCarrinho = async () => {
+        const {adicionarAoCarrinho, servico} = this.props
+
+        const confirmation = window.confirm("Quer adicionar esse serviço ao carrinho?");
+
+        if(!confirmation) return;
+
+        await this.handleTrocarStatus(servico.id, true)
+
+        adicionarAoCarrinho()
+        
+    }
+
     render() {
         const day = new Date(this.props.jobInfos.dueDate).getDay()
         const month = new Date(this.props.jobInfos.dueDate).getMonth()
@@ -14,7 +27,7 @@ export default class CardServico extends React.Component {
                     <p>R${this.props.jobInfos.price.toFixed(2)}</p>
                     <p>{day}/{month}/{year}</p>
                     <button onClick={verDetalhes}>Ver Detalhes</button>
-                    <button>Adicionar no Carrinho</button>
+                    <button onClick={() => this.handleAdicionarAoCarrinho()} >Adicionar no Carrinho</button>
             </CardsServico>
         )
     }
